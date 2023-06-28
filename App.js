232b72295c910bb07ce58fac20eb26738d4be1df
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-06-27 20:06:41 -0400

    Change: add Routes

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainPage from './src/pages/Main.page';
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const change=()=>{
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name="Home" component={MainPage}
+            options={{
+              headerTitleAlign: 'center',
+              headerStyle:{
+                backgroundColor: '#1f2a44',
+                
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                
+              },
+            }}
+          />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
