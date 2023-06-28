d43d760223cd7005a3d7647025e7953f68666309
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-06-28 12:13:07 -0400

    Add: path icons

---
+++ src/constants/imagesPath.js
@@ -0,0 +1,6 @@
+export default {
+    iconDropdown : require('../../assets/images/arrow-icon.png'),
+    iconExpandDropdown : require('../../assets/images/arrow-icon.png'),
+    iconSwap: require('../../assets/icons/swap-arrow.png')
+
+}
