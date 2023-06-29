db4bd3713f72f597d82c7c0a00cd8a61bac2a96b
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-06-28 22:00:36 -0400

    Fix : handled error

--- src/api-calls/apiCalls.js
+++ src/api-calls/apiCalls.js
@@ -1,16 +1,6 @@
 export const getCurrencies = async()=>{
     const url = 'http://data.fixer.io/api/symbols?access_key=482c3c65a5c90bde26b3297d67f9eb83';
-    return new Promise(function (resolve, reject) {
-        fetch(url,{
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(response => resolve(response.symbols))
-        .then(response => console.table(response))
-        .catch(error =>{reject(error)})
-    });
-
+    const response = await fetch(url) ;
+    return response.json() ;
 
 }
