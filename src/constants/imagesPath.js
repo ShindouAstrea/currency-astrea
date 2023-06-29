c11bd8e96892b24473f6005c9bc6d75681dcb745
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-06-28 22:00:14 -0400

    Change: icons path

--- src/constants/imagesPath.js
+++ src/constants/imagesPath.js
@@ -1,6 +1,6 @@
 export default {
-    iconDropdown : require('../../assets/images/arrow-icon.png'),
-    iconExpandDropdown : require('../../assets/images/arrow-icon.png'),
+
+    iconExpandDropdown : require('../../assets/icons/expand-arrow.png'),
     iconSwap: require('../../assets/icons/swap-arrow.png')
 
 }
